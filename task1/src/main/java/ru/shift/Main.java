0ffad7a49d98bc98f314055d8bc9ded991f9995b
Хитринцева Валерия <valeri_valeria@mail.ru>
2024-10-06 22:47:55 +0600

    Вынесены константы, изменены названия переменных, вывод шапки таблицы вынесен в функцию printHeadMultiplicationTable(PrintWriter writer, int tableSize, int maxFieldWidth, String stringDelimiter)

--- task1/src/main/java/ru/shift/Main.java
+++ task1/src/main/java/ru/shift/Main.java
@@ -1,56 +1,72 @@
 package ru.shift;
 
+import java.io.PrintWriter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+    public static final int MIN_TABLE_SIZE = 1;
+    public static final int MAX_TABLE_SIZE = 32;
+    public static final String PLUS = "+";
+    public static final String MINUS = "-";
+    public static final String SPACE = " ";
+
     public static void main(String[] args) {
-        try {
-            int number = enterNumber();
+        try (PrintWriter writer = new PrintWriter(System.out)) {
+            int tableSize;
+            do {
+                tableSize = readTableSize();
+                if (tableSize >= MIN_TABLE_SIZE && tableSize <= MAX_TABLE_SIZE) {
+                    int minFieldWidth = String.valueOf(tableSize).length();
+                    int maxFieldWidth = String.valueOf(tableSize * tableSize).length();
+                    String stringDelimiter = createDelimiter(tableSize, minFieldWidth, maxFieldWidth);
 
-            int minFieldWidth = String.valueOf(number).length();
-            int maxFieldWidth = String.valueOf(number * number).length();
-            String stringDelimiter = createDelimiter(number, minFieldWidth, maxFieldWidth);
-
-            if (number >= 1 && number <= 32) {
-                printMultiplicationTable(number, minFieldWidth, maxFieldWidth, stringDelimiter);
-            } else {
-                System.out.println("Введенное значение не является допустимым: выход за рамки диапазона.");
-            }
+                    printMultiplicationTable(writer, tableSize, maxFieldWidth, stringDelimiter);
+                } else {
+                    writer.println("Введенное значение не является допустимым: выход за рамки диапазона.");
+                }
+            } while (tableSize < MIN_TABLE_SIZE || tableSize > MAX_TABLE_SIZE);
         } catch (InputMismatchException e) {
             System.out.println("Введенное значение не является допустимым: введено не целое число.");
         }
     }
 
-    static int enterNumber() {
-        System.out.println("Введите целочисленное значение 1 <= n <= 32");
+    private static int readTableSize() {
+        System.out.println("Введите целочисленное значение " + MIN_TABLE_SIZE + " <= n <= " + MAX_TABLE_SIZE);
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
 
-    static String createDelimiter(int number, int minFieldWidth, int maxFieldWidth) {
-        StringBuilder stringDelimiter = new StringBuilder(" ".repeat(maxFieldWidth - minFieldWidth) + "-".repeat(minFieldWidth) + "+");
-        String singleDelimiter = "-".repeat(maxFieldWidth) + "+";
-        stringDelimiter.append(singleDelimiter.repeat(number)).deleteCharAt(stringDelimiter.length() - 1);
+    static String createDelimiter(int tableSize, int minFieldWidth, int maxFieldWidth) {
+        int differenceMaxMinFieldWidth = maxFieldWidth - minFieldWidth;
 
-        return stringDelimiter.toString();
+        String zeroDelimiter = SPACE.repeat(differenceMaxMinFieldWidth) + MINUS.repeat(minFieldWidth);
+        String singleDelimiter = PLUS + MINUS.repeat(maxFieldWidth);
+        //int capacity = differenceMaxMinFieldWidth + minFieldWidth + singleDelimiter.length() * tableSize;
+
+        return zeroDelimiter + singleDelimiter.repeat(tableSize);
     }
 
-    static void printMultiplicationTable(int number, int minFieldWidth, int maxFieldWidth, String stringDelimiter) {
-        for (int i = 0; i <= number; i++) {
-            var insert = (i == 0) ? " ".repeat(maxFieldWidth - minFieldWidth) : i;
 
-            System.out.printf("%" + maxFieldWidth + "s" + "|", insert);
+    static void printHeadMultiplicationTable(PrintWriter writer, int tableSize, int maxFieldWidth, String stringDelimiter) {
+        System.out.print(SPACE.repeat(maxFieldWidth) + "|");
+        for (int i = 1; i <= tableSize; i++) {
+            String valueDelimiter = (i == tableSize) ? "\n" : "|";
+            writer.printf("%" + maxFieldWidth + "d%s", i, valueDelimiter);
+        }
+        writer.println(stringDelimiter);
+    }
 
-            for (int j = 1; j <= number; j++) {
-                int multiplierI = (i == 0) ? 1 : i;
-                System.out.printf("%" + maxFieldWidth + "d", multiplierI * j);
-                if (j < number) {
-                    System.out.print("|");
-                }
+    static void printMultiplicationTable(PrintWriter writer, int tableSize, int maxFieldWidth, String stringDelimiter) {
+        printHeadMultiplicationTable(writer, tableSize, maxFieldWidth, stringDelimiter);
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 0; j <= tableSize; j++) {
+                int multiplierJ = (j == 0) ? 1 : j;
+                String valueDelimiter = (j == tableSize) ? "\n" : "|";
+
+                writer.printf("%" + maxFieldWidth + "d%s", multiplierJ * i, valueDelimiter);
             }
-            System.out.println();
-            System.out.println(stringDelimiter);
+            writer.println(stringDelimiter);
         }
     }
 }
