5285e6a2daa5f72cab8f650a69f6f0c60d090457
Хитринцева Валерия <valeri_valeria@mail.ru>
2024-11-26 14:01:19 +0600

    task5 - исправлен порядок в названиях, исправлено наименование констант, длясоздания ApplicationProperties подключен lombok

--- task5/src/main/java/ru/shift/Main.java
+++ task5/src/main/java/ru/shift/Main.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import ru.shift.consumer.Consumer;
 import ru.shift.producer.Producer;
 import ru.shift.storage.Storage;
-import ru.shift.utils.PropertiesProgram;
+import ru.shift.utils.ApplicationProperties;
 import ru.shift.utils.exception.ParsePropertiesException;
 
 import java.io.IOException;
@@ -17,24 +17,32 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        PropertiesProgram propertiesProgramm;
-        try {
-            propertiesProgramm = loadProperties();
-        } catch (ParsePropertiesException | IOException e) {
-            logger.error(String.valueOf(e));
-            propertiesProgramm = new PropertiesProgram(storageSize,
-                    producerCount, consumerCount,
-                    producerTime, consumerTime,
-                    nameProducer, nameConsumer);
-            logger.info("The program started with default values");
-        }
+        ApplicationProperties applicationProperties = getPropertiesProgram();
 
-        Storage storage = new Storage(propertiesProgramm.storageSize());
-        for (int i = 0; i < propertiesProgramm.producerCount(); i++) {
-            new Thread(new Producer(storage, propertiesProgramm.producerTime()), propertiesProgramm.nameProducer() + i).start();
+        Storage storage = new Storage(applicationProperties.storageSize());
+        for (int i = 0; i < applicationProperties.producerCount(); i++) {
+            new Thread(new Producer(storage, applicationProperties.producerTime()), applicationProperties.producerName() + i).start();
         }
-        for (int i = 0; i < propertiesProgramm.consumerCount(); i++) {
-            new Thread(new Consumer(storage, propertiesProgramm.consumerTime()), propertiesProgramm.nameConsumer() + i).start();
+        for (int i = 0; i < applicationProperties.consumerCount(); i++) {
+            new Thread(new Consumer(storage, applicationProperties.consumerTime()), applicationProperties.consumerName() + i).start();
+        }
+    }
+
+    private static ApplicationProperties getPropertiesProgram() {
+        try {
+            return loadProperties();
+        } catch (ParsePropertiesException | IOException e) {
+            logger.error("Error while reading properties", e);
+            logger.info("The program started with default values");
+            return ApplicationProperties.builder()
+                    .storageSize(STORAGE_SIZE)
+                    .producerCount(PRODUCER_COUNT)
+                    .consumerCount(CONSUMER_COUNT)
+                    .producerTime(PRODUCER_TIME)
+                    .consumerTime(CONSUMER_TIME)
+                    .producerName(PRODUCER_NAME)
+                    .consumerName(CONSUMER_NAME)
+                    .build();
         }
     }
 }
